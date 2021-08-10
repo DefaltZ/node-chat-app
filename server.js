b623139a52814e7ba8345a67bbc940bfc4eef73d
DefaltZ <priyongshupaul@gmail.com>
2021-08-10 19:50:28 +0530

    creating public html files

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express  = require('express');
 const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app);
+const io = socketio(server);
 
 //SET STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')))
 
+io.on('connection', socket => {
+    console.log('new socket connection...')
+})
 const PORT = 9000 || process.env.PORT
-
-app.listen(PORT, () => console.log(`chat server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`chat server running on port ${PORT}`))
