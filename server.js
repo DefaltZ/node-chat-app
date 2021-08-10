ba9e12b47ee60524253f21f8143f704e12622d02
DefaltZ <priyongshupaul@gmail.com>
2021-08-10 22:31:14 +0530

    added chat features

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 io.on('connection', socket => {
     console.log('new socket connection...')
-})
+
+    socket.emit('message', 'welcome to node chat app')
+    //broadcast to others when user connects
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    //disconnect user
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat')
+    })
+});
 const PORT = 9000 || process.env.PORT
 server.listen(PORT, () => console.log(`chat server running on port ${PORT}`))
