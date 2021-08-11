f6c9ae7ea6d96b503e9aec057aa0d4344049d7fa
DefaltZ <priyongshupaul@gmail.com>
2021-08-11 15:00:37 +0530

    improved chat features

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express  = require('express');
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/message')
+const botName = 'node chatbot'
 
 const app = express()
 const server = http.createServer(app);
@@ -13,18 +15,18 @@ app.use(express.static(path.join(__dirname, 'public')))
 io.on('connection', socket => {
     console.log('new socket connection...')
 
-    socket.emit('message', 'welcome to node chat app')
+    socket.emit('message', formatMessage(botName, 'welcome to node chat app'))
     //broadcast to others when user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     //disconnect user
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat')
+        io.emit('message', formatMessage(botName, 'A user has left the chat'))
     })
 
     //listen for chat message
     socket.on('chatmessage', (msg) => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 });
 const PORT = 9000 || process.env.PORT
